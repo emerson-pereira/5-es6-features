bb095836b7e8593377547106b9f4244f1f2bef7f
Emerson Pereira <emerson.mdca@gmail.com>
2019-03-22 18:18:28 -0300

    Create template-string-and-destructuring.js

---
+++ features/template-string-and-destructuring.js
@@ -0,0 +1,10 @@
+const user = {
+	name: 'Emerson',
+	age: 23
+};
+
+const { name, age } = user;
+
+const message = `Your name is ${name} and you are ${age} years old`;
+
+// "Your name is Emerson and you are 23 years old"
